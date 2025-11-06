5d2d50ed269fd3e9dcc866fabdbe7d5b8e41319d
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-11-06 09:08:46 +0100

    update

--- config.py
+++ config.py
@@ -69,9 +69,9 @@ CONFIG = {
     #    and a smaller, more stable learning rate.
     # User requested change: Increase epochs
     'lr': 1e-4,                     # Was: 3e-4
-    'epochs': 15,                   # Was: 4, then 10
+    'epochs': 20,                   # Was: 4, then 10
     'episodes_per_epoch': 500,      # Was: 200
-    'episodic_label_shuffle': True, # Set to True to enable label shuffling augmentation
+    'episodic_label_shuffle': False, # Set to True to enable label shuffling augmentation
 
     # --- Test Parameters ---
     'num_test_episodes': 200,
