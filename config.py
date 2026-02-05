c9cf2881c7d8a659f6d7ce3c3e23cd56717dfe1c
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2026-02-05 11:08:16 +0100

    update

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ CONFIG ={
 'retrieval_train_batch_size':64 ,
 'test_mode':'retrieval_augmented',
 'test_retrieval_k':[1 ,5 ,10 ,20 ],
-'test_retrieval_candidate_percentages':[1 ,5 ,20 ],
+'test_retrieval_candidate_percentages':[0.5 ,1 ,3 ,5 ],
 'num_test_episodes':200 ,
 'num_cases_for_testing':500 ,
 }
